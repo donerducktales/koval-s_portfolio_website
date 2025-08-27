4bd260c6681e48c6adc8399ee0c591ec5b13ac08
Koval Sviatoslav <randomercook231@gmail.com>
2025-08-27 11:44:18 +0300

    початок, стер все лишнє для початку розробки, додав шрифти та скачав motion для анімацій, zustand та next-intl для інтернаціоналізації

---
+++ src/assets/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter, Montserrat } from "next/font/google";
+
+export const inter = Inter({
+  subsets: ["latin", "cyrillic"],
+  weight: ["400"],
+});
+
+export const montserrat = Montserrat({
+  subsets: ["latin", "cyrillic"],
+  weight: ["400"],
+});
